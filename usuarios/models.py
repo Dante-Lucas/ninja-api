a0d00751a2ef64f8028a399d06413215758d8076
Dante-Lucas <157182980+Dante-Lucas@users.noreply.github.com>
2024-09-19 10:22:06 -0300

    Add files via upload

--- usuarios/models.py
+++ usuarios/models.py
@@ -3,9 +3,14 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 
 class User(AbstractUser):
+    user_id = models.BigAutoField(primary_key=True, editable=False,auto_created=True, serialize=False, verbose_name='ID')
     username = models.CharField(max_length=50, unique=True)
     email = models.EmailField(max_length=254, unique=True)
 
 
     EMAIL_FIELD = "email"
     USERNAME_FIELD = "username"
+    class Meta:
+        verbose_name = "user"
+        verbose_name_plural = "users"
+        db_table = "User"
